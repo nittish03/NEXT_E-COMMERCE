02454c5a1d80414a5bf39423980b0ba60be2d9a6
nittish03 <nittishbaboria123@gmail.com>
2025-07-19 20:17:23 +0530

    final code

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -73,7 +73,7 @@ const Page = () => {
         getTotalProducts();
       }
     }
-  }, [session, cartCount, setCartCount, addToCart,filteredProducts,totalPrice]);
+  }, [session, cartCount]);
 
   return (
     <>
